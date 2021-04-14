fd0b43b3e986e7e119561a9e0c045037e0dd0f29
Johannes Rieke <johannes.rieke@gmail.com>
2021-04-14 23:17:34 +0200

    check keys

--- s3_app.py
+++ s3_app.py
@@ -1,10 +1,13 @@
 import streamlit as st
 import s3fs
 import time
+import os
+
+st.write(os.environ["aws_access_key_id"][:5])
 
 start_time = time.time()
 
-# Create connection object. 
+# Create connection object.
 # `anon=False` means not anonymous, i.e. it uses auth to pull data.
 fs = s3fs.S3FileSystem(anon=False)
 
@@ -12,7 +15,7 @@ f"Creating fs took {time.time() - start_time} s"
 
 start_time_read = time.time()
 
-# Retrieve file contents and write to streamlit app. 
+# Retrieve file contents and write to streamlit app.
 with fs.open("testbucket-jrieke/myfile.csv") as f:
     for line in f:
         name, pet = line.decode("utf-8").strip().split(",")
