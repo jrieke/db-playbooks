c0217337843c3d3a26b6cc88a979505acbb16201
Johannes Rieke <johannes.rieke@gmail.com>
2021-05-07 22:38:46 +0200

    Added example for private google sheet

---
+++ sheets_private_app.py
@@ -0,0 +1,32 @@
+# streamlit_app.py
+
+import json
+import streamlit as st
+from gsheetsdb import connect
+from google.oauth2 import service_account
+
+
+# Create a connection object.
+google_key_json = json.loads(st.secrets["google_key"], strict=False)
+credentials = service_account.Credentials.from_service_account_info(
+    google_key_json,
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+conn = connect(credentials=credentials)
+
+# Perform SQL query on the Google Sheet.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    return rows
+
+
+sheet_url = st.secrets["private_gsheets_url"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+# Print results.
+for row in rows:
+    st.write(f"{row.name} has a :{row.pet}:")
